707f5e8a2895546e04a64aeb4e9beeea4744f71d
TomTomCoffey <tomcoffey1017@yahoo.com>
2024-07-08 23:36:10 -0400

    Working through service layer starting with category and modifiers

--- server/src/main/java/learn/domain/CategoryService.java
+++ server/src/main/java/learn/domain/CategoryService.java
@@ -4,6 +4,8 @@ import learn.data.CategoryRepository;
 import learn.models.Category;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CategoryService {
 
@@ -13,6 +15,58 @@ public class CategoryService {
     public CategoryService(CategoryRepository repository) {
         this.repository = repository;
 
+    }
+
+    public List<Category> findAll(){
+        return repository.findAll();
+    }
+
+
+
+    public Result<Category> add(Category category){
+
+        Result<Category> result = new Result<>();
+
+        if(category.getName() == null || category.getName().isEmpty() || category.getName().isBlank()){
+            result.addMessage("Category must have valid name", ResultType.INVALID);
+            return result;
+        }
+        Category category1 = repository.add(category);
+
+        if(category1 == null){
+            result.addMessage("Error when adding Category to database", ResultType.NOT_FOUND);
+            return result;
+        }
+
+        result.setPayload(category);
+
+        return result;
+
+
+    }
+
+    public Result<Category> update(Category category){
+        Result<Category> result = new Result<>();
+
+        if(category.getName() == null || category.getName().isEmpty() || category.getName().isBlank()){
+            result.addMessage("Category must have valid name", ResultType.INVALID);
+            return result;
+        }
+
+        boolean expected = repository.update(category);
+
+        if(!expected){
+            result.addMessage("An error has occured please try updating again", ResultType.INVALID);
+            return result;
+        }
+
+        result.setPayload(category);
+        return result;
+
+    }
+
+    public boolean deleteById(int id){
+        return repository.deletedById(id);
     }
 
 
---
+++ server/src/main/java/learn/domain/ModifiersService.java
@@ -0,0 +1,82 @@
+package learn.domain;
+
+import learn.data.ModifiersRepository;
+import learn.models.Modifiers;
+import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Modifier;
+import java.util.List
+
+@Service
+public class ModifiersService {
+
+    private final ModifiersRepository repository;
+
+    public ModifiersService(ModifiersRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Modifiers> findAll(){
+        return repository.findAll();
+    }
+
+    public List<Modifiers> findByItemId(int itemId){
+        return repository.findByItemId(itemId);
+    }
+
+    public Result<Modifiers> add(Modifiers modifiers){
+        Result<Modifiers> result = validate(modifiers);
+
+        if(!result.isSuccess()){
+            return result;
+        }
+        Modifiers modifiers1 = repository.add(modifiers);
+
+        if(modifiers1 == null){
+            result.addMessage("An Error occured in the repository", ResultType.INVALID);
+            return result;
+        }
+        result.setPayload(modifiers1);
+        return result;
+
+
+    }
+    public Result<Modifiers> update(Modifiers modifiers){
+        Result<Modifiers> result = validate(modifiers);
+
+        if(!result.isSuccess()){
+            return result;
+        }
+        Modifiers m = repository.findAll().stream()
+                .filter(m -> m.getModifier_id() == modifiers.getModifier_id())
+                .findFirst()
+                .orElse(null);
+
+        if(m == null){
+            result.addMessage("Cannot find item in database to update", ResultType.NOT_FOUND);
+            return result;
+        }
+        boolean expected = repository.update(modifiers);
+        if(!expected){
+            result.addMessage("An error occured in the repository", ResultType.NOT_FOUND);
+            return result
+        }
+        result.setPayload(modifiers);
+        return result;
+    }
+
+    private Result<Modifiers> validate(Modifiers modifiers){
+        Result<Modifiers> result = new Result<>();
+
+        if(modifiers.getName() == null || modifiers.getName().isEmpty() || modifiers.getName().isBlank()){
+            result.addMessage("Modifiers must have a valid name", ResultType.INVALID);
+
+        }
+        if(modifiers.getPrice().doubleValue() < 0){
+            result.addMessage("Modifiers cannot have negative price", ResultType.INVALID);
+
+        }
+
+        return result;
+    }
+}

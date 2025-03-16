881ea339c75c3eeeb9f4413158591ebe81862580
TomTomCoffey <tomcoffey1017@yahoo.com>
2025-03-16 19:21:02 -0400

    updating items now functional will need to maintain state on table outside modal

--- client/src/components/EditDeleteItemTable.js
+++ client/src/components/EditDeleteItemTable.js
@@ -1,75 +1,138 @@
-import { Container, getBottomNavigationActionUtilityClass, Table, TableBody, TableCell, TableHead, TableRow, Button } from "@mui/material";
+import {
+  Container,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Button,
+  Modal,
+  Box,
+} from "@mui/material";
 import axios from "axios";
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 import { toast } from "react-toastify";
+import EditItemModal from "./EditItemModal";
 
-function EditDeleteItemTable(){
+function EditDeleteItemTable() {
+  const [items, setItems] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [item, setItem] = useState({}); 
 
-    const [items, setItems] = useState([]);
-    const [checker, setChecker] = useState(false);
-    
-    useEffect(()=>{
-        const getMenu = async () => {
-            try{
-                const response = await axios.get('http://localhost:8080/api/item');
-                    setItems(response.data);      
-            }catch(error){
-                toast.error(error);
-            }
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    const getMenu = async () => {
+      try {
+        const response = await axios.get("http://localhost:8080/api/item");
+        setItems(response.data);
+      } catch (error) {
+        toast.error(error);
+      }
+    };
+    getMenu();
+  }, []);
+
+  const deleteItem = async (item) => {
+    const isConfirmed = window.confirm(
+      "Are you sure you want to delete this item?"
+    );
+
+    if (isConfirmed) {
+      console.log(item.itemId);
+      try {
+        const response = await axios.delete(
+          `http://localhost:8080/api/item/${item.itemId}`
+        );
+        if (response.status === 204) {
+          toast.success(`${item.title} was successfully deleted`);
+          setItems(items.filter((i) => i.itemId !== item.itemId));
         }
-        getMenu();
-    },[])
-
-
-    const deleteItem = async (item) => {
-
-        const isConfirmed = window.confirm("Are you sure you want to delete this item?");
-
-        if(isConfirmed){
-            console.log(item.itemId);
-            try{
-                const response = await axios.delete(`http://localhost:8080/api/item/${item.itemId}`);
-                if(response.status === 204){
-                    toast.success(`${item.title} was successfully deleted`);
-                    setItems(items.filter(i => i.itemId !== item.itemId));
-                }
-            }catch(error){
-                toast.error(error);
-            
-            } 
-        }
+      } catch (error) {
+        toast.error(error);
+      }
     }
+  };
 
+  const openEditItemModal = (item) => {
+    setItem(item);
+    handleOpen();
+  };
 
-    return(
-        <>
-        <Container>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Name</TableCell>
-                        <TableCell>Description</TableCell>
-                        <TableCell>Price</TableCell>
-                        <TableCell>Edit</TableCell>
-                        <TableCell>Delete</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                  {items.map((item)=> 
-                  <TableRow>
-                    <TableCell>{item.title}</TableCell>
-                    <TableCell>{item.description}</TableCell>
-                    <TableCell>{item.price}</TableCell>
-                    <TableCell><Button variant="outlined" color="primary">Edit</Button></TableCell>
-                    <TableCell><Button varient="outlined" color="error" onClick={() => deleteItem(item)}>Delete</Button></TableCell>
-                  </TableRow>
-                )}
-                </TableBody>
-            </Table>
-        </Container>
-
-        </>
-    )
+  return (
+    <>
+      <Container>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="child-modal-title"
+          aria-describedby="child-modal-description"
+          scrollBehavior="auto"
+        >
+          <Box
+            sx={{
+              position: "absolute",
+              scrollBehavior: "auto",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              width: 600,
+              bgcolor: "background.paper",
+              border: "2px solid #000",
+              boxShadow: 24,
+              p: 4,
+              borderRadius: 2,
+            }}
+          >
+            <EditItemModal oldItem={item}></EditItemModal>
+          </Box>
+        </Modal>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Description</TableCell>
+              <TableCell>Price</TableCell>
+              <TableCell>Edit</TableCell>
+              <TableCell>Delete</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {items.map((item) => (
+              <TableRow>
+                <TableCell>{item.title}</TableCell>
+                <TableCell>{item.description}</TableCell>
+                <TableCell>{item.price}</TableCell>
+                <TableCell>
+                  <Button
+                    variant="outlined"
+                    color="primary"
+                    onClick={() => openEditItemModal(item)}
+                  >
+                    Edit
+                  </Button>
+                </TableCell>
+                <TableCell>
+                  <Button
+                    varient="outlined"
+                    color="error"
+                    onClick={() => deleteItem(item)}
+                  >
+                    Delete
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Container>
+    </>
+  );
 }
 
 export default EditDeleteItemTable;
---
+++ client/src/components/EditItemModal.js
@@ -0,0 +1,128 @@
+import React from "react";
+import { Container, FormGroup, TextField, Select, MenuItem, Button, Autocomplete, Chip} from "@mui/material";
+import { useState, useEffect } from "react";
+import { toast } from "react-toastify";
+import axios from "axios";
+
+
+function EditItemModal({oldItem}){
+
+        const [categories, setCategories] = useState([]);
+        const [modifiers, setModifiers] = useState([]);
+        const [selectedModifiers, setSelectedModifiers] = useState(oldItem.modifiers);
+        const [item, setItem] = useState(oldItem);
+
+
+
+        const handleChange = (event) => {
+            const {name, value} = event.target;
+    
+            setItem(prevItem => ({
+                ...prevItem,
+                [name]: value
+            }))
+    
+        }
+    
+        const handleSubmit = async (e) => {
+            e.preventDefault();
+            try{
+             const newItem = {...item, modifiers:selectedModifiers}   
+            const response = await axios.put(`http://localhost:8080/api/item/id/${item.itemId}`, newItem);
+            if(response.status === 204){
+                toast.success("Changes successfully Made");
+            }
+          
+            }catch(error){
+                if(error.response){
+                const errors = error.response.data;
+                for(let i = 0; i < errors.length; i++){
+                    toast.error(errors[i]);
+                }
+            }
+            }    
+        }
+        useEffect(() => {
+            const fetchCategories = async () => {
+                try {
+                    const response = await axios.get('http://localhost:8080/api/category');
+                        setCategories(response.data);
+                } catch (error) {
+                    toast.error(error);
+                }
+            };
+            fetchCategories();
+        }, []);
+    
+        useEffect(() => {
+            const fetchModifiers = async () => {
+                try {
+                    const response = await axios.get('http://localhost:8080/api/modifiers');   
+                        setModifiers(response.data);
+                } catch (error) {
+                    toast.error(error);
+                }
+            };
+            fetchModifiers();
+        }, []);
+
+
+    return(
+        <>
+        <h1>Edit {item.title}</h1>
+        <Container maxWidth="sm">
+                <form>
+                    <FormGroup sx={{ marginBottom: 2, marginTop: 6 }}>
+                        <TextField label="Item Name" fullWidth name='title' value={item.title} onChange={handleChange}/>
+                    </FormGroup>
+
+                    <FormGroup sx={{ marginBottom: 2 }}>
+                        <TextField label="Description" multiline fullWidth name='description' value={item.description} onChange={handleChange}/>
+                    </FormGroup>
+
+                    <FormGroup sx={{ marginBottom: 2 }}>
+                        <TextField type="number" label="Price" fullWidth name='price' value={item.price}onChange={handleChange}/>
+                    </FormGroup>
+
+                    <FormGroup sx={{ marginBottom: 2 }}>
+                        <Select displayEmpty fullWidth 
+                                name="category"
+                                value={item.category || "is this working"}
+                                 onChange={handleChange}>
+                            <MenuItem value={item.category} disabled>{item.category.name}</MenuItem>
+                            {categories.map((category, index) => (
+                                <MenuItem key={index} value={category}>{category.name}</MenuItem>
+                            ))}
+                        </Select>
+                    </FormGroup>
+
+                    <FormGroup sx={{ marginBottom: 2 }}>
+                        <Autocomplete
+                            multiple
+                            options={modifiers}
+                            getOptionLabel={(mod) => `${mod.name} - $${mod.price}`}
+                            value={selectedModifiers}
+                            onChange={(event, newValue) => setSelectedModifiers(newValue)}
+                            renderInput={(params) => <TextField {...params} label="Select Modifiers" fullWidth />}
+                            renderTags={(selected, getTagProps) =>
+                                selected.map((option, index) => (
+                                    <Chip
+                                        key={index}
+                                        label={`${option.name} - $${option.price}`}
+                                        {...getTagProps({ index })}
+                                    />
+                                ))
+                            }
+                            sx={{ width: "100%" }}
+                        />
+                    </FormGroup>
+                    <FormGroup sx={{ marginBottom: 2 }}>
+                        <Button variant="outlined" color="primary" type='submit' onClick={handleSubmit}>Submit</Button>
+                    </FormGroup>
+                </form>
+            </Container>
+        </>
+    )
+}
+
+export default EditItemModal;
